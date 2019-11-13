fa68291bb4bef4763f69b0d3e3e21a977b839f58
Robert Nordgren <robert.b.nordgren@gmail.com>
2019-11-13 10:19:59 +0100

    Added mock database for customers and accounts Added customer and accounts listing on startpage

---
+++ ALM_Assignment1/Data/BankRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALM_Assignment1.Models
+{
+    public interface IBankRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+        IEnumerable<Account> GetAccounts();
+    }
+
+    public class BankRepository : IBankRepository
+    {
+        public static IEnumerable<Customer> Customers = new List<Customer>
+        {
+            new Customer { CustomerID = 1, Name = "Steve Tabernackle" },
+            new Customer { CustomerID = 2, Name = "Sarah Andrews" },
+            new Customer { CustomerID = 3, Name = "Donna Carter" },
+        };
+
+        public IEnumerable<Account> Accounts = new List<Account>
+        {
+            new Account { AccountID = 1, CustomerID = 1, Balance = 25543M },
+            new Account { AccountID = 2, CustomerID = 2, Balance = 8403M },
+            new Account { AccountID = 3, CustomerID = 3, Balance = 323M },
+            new Account { AccountID = 4, CustomerID = 1, Balance = 4235662M },
+            new Account { AccountID = 5, CustomerID = 2, Balance = 47034M },
+        };
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return Accounts;
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return Customers;
+        }
+    }
+}
---
+++ ALM_Assignment1/Domain/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALM_Assignment1.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public int CustomerID { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
